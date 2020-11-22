07a51b0557d735d77dad9f6bc0936c56e4a51bb7
Jack2ee-dev <jack2ee.dev@gmail.com>
2020-11-22 17:25:45 +0900

    REFACTOR: 기능 1

--- src/main/java/RandomNumbers.java
+++ src/main/java/RandomNumbers.java
@@ -3,6 +3,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class RandomNumbers {
+    private int RANDOM_NUMBER_START = 1;
+    private int RANDOM_NUMBER_END = 9;
+    private int RANDOM_NUMBER_SIZE = 3;
+
     List<Integer> randomNumbers;
 
     public RandomNumbers() {
@@ -11,7 +15,7 @@ public class RandomNumbers {
 
     private List<Integer> generateNumberListBetweenOneAndNine() {
         List<Integer> numbers = new ArrayList<>();
-        for (int i = 1; i < 10; i++) {
+        for (int i = RANDOM_NUMBER_START; i <= RANDOM_NUMBER_END; i++) {
             numbers.add(i);
         }
         return numbers;
@@ -19,10 +23,9 @@ public class RandomNumbers {
 
     private List<Integer> selectThreeNumbersRandomly(List<Integer> numbers) {
         List<Integer> threeNumbers = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            Collections.shuffle(numbers);
-            threeNumbers.add(numbers.get(0));
-            numbers.remove(0);
+        Collections.shuffle(numbers);
+        for (int i = 0; i < RANDOM_NUMBER_SIZE; i++) {
+            threeNumbers.add(numbers.get(i));
         }
         return threeNumbers;
     }
